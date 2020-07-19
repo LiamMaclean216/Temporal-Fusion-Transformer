20cf1ae0649eaef10860f1161ad4eac0bbab9664
Liam Maclean <lmaclean234@gmail.com>
2020-07-19 17:33:34 -0400

    TFN architecture

--- Network.py
+++ Network.py
@@ -17,7 +17,6 @@ class GRN(torch.nn.Module):
     def __init__(self, dim_input, dim_out = None, n_hidden = 10, dropout_r = 0.1):
         super(GRN, self).__init__()
         
-        
         if(dim_out != None):
             self.skip = nn.Linear(dim_input, dim_out)
         else:
@@ -43,25 +42,53 @@ class GRN(torch.nn.Module):
         return self.norm(x + a)
 
 class VSN(torch.nn.Module):
-    def __init__(self, n_variables, dim_model, dropout_r = 0.1):
+    def __init__(self, n_var_cont, n_var_disc, dim_model, dropout_r = 0.1):
         super(VSN, self).__init__()
+        n_var_total = n_var_cont + len(n_var_disc)
         
         #Linear transformation of inputs into dmodel vector
-        self.fc = nn.Linear(1, dim_model, bias = False)
+        self.linearise = []
+        for i in range(n_var_cont):
+            self.linearise.append(nn.Linear(1, dim_model, bias = False).cuda())
+        self.fc = nn.Linear(1, dim_model, bias = False).cuda()
+        
+        #entity embedings for discrete inputs
+        self.entity_embed = []
+        for i in n_var_disc:
+            self.entity_embed.append(nn.Linear(i, dim_model, bias = False).cuda())
+        
         
         self.input_grn = GRN(dim_model, dropout_r = dropout_r)
-        self.vs_grn = GRN(n_variables * dim_model * 1, dim_out = n_variables, dropout_r = dropout_r)
+        self.vs_grn = GRN(n_var_total * dim_model, dim_out = n_var_total, dropout_r = dropout_r)
     
     #takes input (batch_size, seq_len, n_variables, input_size)
-    def forward(self, x):
-        linearised = self.fc(x.unsqueeze(-1))
+    def forward(self, x_cont, x_disc):
+        #linearise continuous inputs
+        linearised = []
+        for idx, fc in enumerate(self.linearise):
+            linearised.append(fc(x_cont[:,:,idx]))
+        
+        #entity embeddings for discrete inputs
+        embedded = []
+        for x, fc in zip(x_disc, self.entity_embed):
+            embedded.append(fc(x))
+        
+        if(len(self.linearise) != 0 and len(self.entity_embed) != 0):
+            linearised = torch.stack(linearised, dim = -2)
+            embedded = torch.stack(embedded, dim = -2)
+            vectorised_vars = torch.cat((linearised, embedded), dim = -2) #(batch_size, seq_len, dim_model, n_vars_total)
+        elif(len(self.linearise) != 0 and len(self.entity_embed) == 0):
+            vectorised_vars = torch.stack(linearised, dim = -2) #(batch_size, seq_len, n_var_cont, dim_model)
+        elif(len(self.entity_embed) != 0 and len(self.linearise) == 0):
+            vectorised_vars = torch.stack(embedded, dim = -2)#(batch_size, seq_len, n_var_disc, dim_model)
+        
         
         #flatten everything except accross batch for variable selection weights
-        vs_weights = self.vs_grn(linearised.flatten(start_dim = 2)) #(batch_size, seq_len, n_variables)
+        vs_weights = self.vs_grn(vectorised_vars.flatten(start_dim = 2)) #(batch_size, seq_len, n_variables)
         vs_weights = torch.softmax(vs_weights, dim = -1).unsqueeze(-1) #(batch_size, seq_len, n_variables, 1)
         
         #input_grn applied to every input seperatly
-        input_weights = self.input_grn(linearised) #(batch_size, seq_len, n_variables, dim_model)
+        input_weights = self.input_grn(vectorised_vars) #(batch_size, seq_len, n_variables, dim_model)
         
         x = torch.sum((vs_weights * input_weights), dim = 2)
         return x #returns(batch_size, seq_len, dim_model)
@@ -89,7 +116,81 @@ class LSTMLayer(torch.nn.Module):
         
         return x, self.hidden #returns (batch_size, seq_len, dim_model), hidden
     
-    def reset(self, batch_size):
-        self.hidden = (torch.zeros([self.n_layers, batch_size, self.dim_model]),
-                   torch.zeros([self.n_layers, batch_size, self.dim_model]))
+    def reset(self, batch_size, gpu = True):
+        if(not gpu):
+            dtype = torch.FloatTensor
+        else:
+            dtype = torch.cuda.FloatTensor
+        self.hidden = (torch.zeros([self.n_layers, batch_size, self.dim_model]).type(dtype),
+                   torch.zeros([self.n_layers, batch_size, self.dim_model]).type(dtype))
+        
+class TFN(torch.nn.Module):
+    def __init__(self, n_var_past_cont, n_var_future_cont, n_var_past_disc, n_var_future_disc
+                 , dim_model, n_quantiles = 3, dropout_r = 0.1, n_lstm_layers = 1, n_attention_layers = 1, n_heads = 4):
+        super(TFN, self).__init__()
+        self.vs_past = VSN(n_var_past_cont, n_var_past_disc, dim_model, dropout_r = dropout_r)
+        self.vs_future = VSN(n_var_future_cont, n_var_future_disc, dim_model, dropout_r = dropout_r)
+        
+        self.enc = LSTMLayer(dim_model, dropout_r = dropout_r, n_layers = n_lstm_layers)
+        self.dec = LSTMLayer(dim_model, dropout_r = dropout_r, n_layers = n_lstm_layers)
+        
+        self.gate1 = GLU(dim_model)
+        self.norm1 = nn.LayerNorm(dim_model)
+        
+        self.static_enrich_grn = GRN(dim_model, dropout_r = dropout_r)
+        
+        self.attention = []
+        for i in range(n_attention_layers):
+            self.attention.append(MultiHeadAttentionBlock(dim_model, dim_model, n_heads = n_heads).cuda())
+            
+        self.norm2 = nn.LayerNorm(dim_model)
+        
+        self.positionwise_grn = GRN(dim_model, dropout_r = dropout_r)
+        self.norm3 = nn.LayerNorm(dim_model)
+        
+        self.dropout = nn.Dropout(dropout_r)
+        self.fc_out = nn.Linear(dim_model, n_quantiles)
+        
+    #takes input (batch_size, past_seq_len, n_variables_past)
+    #, (batch_size, future_seq_len, n_variables_future)
+    def forward(self, x_past_cont, x_past_disc, x_future_cont, x_future_disc):
+        #Encoder
+        x_past = self.vs_past(x_past_cont, x_past_disc)
+        
+        e, e_hidden = self.enc(x_past)
+        self.dec.hidden = e_hidden
+        
+        e = self.dropout(e)
+        x_past = self.norm1(self.gate1(e) + x_past)
+        
+        #Decoder
+        x_future = self.vs_future(x_future_cont, x_future_disc)
+        
+        d, _ = self.dec(x_future)
+        d = self.dropout(d)
+        x_future = self.norm1(self.gate1(d) + x_future)
+        
+        #Static enrichment (Without the static for now)
+        x = torch.cat((x_past, x_future), dim = 1) #(batch_size, past_seq_len + future_seq_len, dim_model)
+        attention_res = x_future
+        x = self.static_enrich_grn(x)
+        
+        #attention layer
+        #a = self.attention(x)
+        
+        a = self.attention[0](x)
+        for at in self.attention[1:]:
+            a = at(a)
+        
+        x_future = self.norm2(a[:, x_past.shape[1]:] + x_future)
+        
+        a = self.positionwise_grn(x_future)
+        x_future = self.norm3(a + x_future + attention_res)
+        
+        net_out = self.fc_out(x_future)
+        return net_out
+    
+    def reset(self, batch_size, gpu = True):
+        self.enc.reset(batch_size, gpu)
+        self.dec.reset(batch_size, gpu)        
     
